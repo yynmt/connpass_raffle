4afa71d97e544c248988330c604cade995b89472
yynmt <yynmt@yynmt.com>
2023-02-22 02:25:32 +0900

    2回目以降の起動時に当選者を抽選対象から除外するよう修正
    参加者リストに加え当選者リストの表示機能追加
    ボタンなどのレイアウトを全体的に修正

--- connpass_lottery/main.py
+++ connpass_lottery/main.py
@@ -1,4 +1,5 @@
 import os
+import re
 import random
 import datetime
 import logging
@@ -14,7 +15,12 @@ os.makedirs(Path(LOG_FILE_PATH).parent, exist_ok=True)
 
 app = Flask(__name__)
 participant_list = []
+winner_list = []
 logger = logging.getLogger(__name__)
+
+part_csv_path = r'./data.csv'
+win_csv_path = re.sub(r'\..+$', '_win.csv', str((Path(part_csv_path).absolute())))
+Path(win_csv_path).touch(exist_ok=True)
 
 
 def setup_logger():
@@ -65,8 +71,29 @@ def index():
 
 @app.route('/participant_list')
 def participant_list():
-    part_list = ['{} : {}'.format(p.user_name, p.display_name) for p in participant_list]
-    return render_template('participant_list.html', part_list=part_list)
+    part_list = []
+    for part in participant_list:
+        part_list.append([
+            part.user_name,
+            part.display_name,
+            part.rcpt_number
+        ])
+
+    sum = len(participant_list)
+    return render_template('participant_list.html', part_list=part_list, sum=sum)
+
+
+@app.route('/winner_list')
+def winner_list():
+    part_list = []
+    for part in winner_list:
+        part_list.append([
+            part.user_name,
+            part.display_name,
+            part.rcpt_number
+        ])
+    sum = len(winner_list)
+    return render_template('winner_list.html', part_list=part_list, sum=sum)
 
 
 @app.route('/lottery', methods=['GET', 'POST'])
@@ -82,14 +109,18 @@ def lottery():
             # 当選者をリストからpop
             part = participant_list.pop(idx)
 
+            winner_list.append(part)
+            participant.save(win_csv_path, winner_list)
+
             # ユーザー表示名
             name = part.display_name
             # 受付番号
             num = part.rcpt_number
-
+            # 抽選対象の参加者全員のユーザー表示名
+            all_part_name_list = [p.display_name for p in participant_list]
             logger.debug('{}: {}'.format(num, name))
 
-            return render_template('index.html', number=num, name=name)
+            return render_template('index.html', number=num, name=name, all_part_name_list=all_part_name_list)
         except ValueError:
             return render_template('error.html')
     elif request.method == 'GET':
@@ -102,8 +133,10 @@ if __name__ == '__main__':
     # ロガー設定
     setup_logger()
     # 参加者csv読み込み
-    csv_path = r'./data.csv'
-    participant_list = participant.load(csv_path)
+    participant_list, winner_list = participant.load(part_csv_path, win_csv_path)
 
     app.run()
     # app.run(port=80, debug=False)
+
+# ToDo
+# 当選してpopされたユーザを別のcsvに書き出す
--- connpass_lottery/participant.py
+++ connpass_lottery/participant.py
@@ -4,15 +4,50 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-def load(csv_path=''):
+def load(part_csv_path, win_csv_path):
     """
     connpassからダウンロードできるcsvファイルの読み込み
     """
 
-    tmp_list = []
+    tmp_part_list = []
+    tmp_win_list = []
+
+    # 当選者の読み込み
+    with open(win_csv_path, encoding='cp932') as f:
+        reader = csv.reader(f)
+
+        # 1行ずつ読み込み
+        for row in reader:
+            tmp_part = Participant()
+
+            # 参加枠
+            tmp_part.frame_name = row[0]
+            # ユーザー名
+            tmp_part.user_name = row[1]
+            # 表示名
+            tmp_part.display_name = row[2]
+            # 利用開始日
+            tmp_part.start_date = row[3]
+            # コメント
+            tmp_part.comment = row[4]
+            # 参加ステータス
+            tmp_part.status_part = row[5]
+            # 出欠ステータス
+            tmp_part.status_att = row[6]
+            # PayPal取引ID
+            tmp_part.paypal_id = row[7]
+            # 請求書ID
+            tmp_part.invoice_id = row[8]
+            # 更新日時
+            tmp_part.update_time = row[9]
+            # 受付番号
+            tmp_part.rcpt_number = row[10]
+
+            # リストに参加者を追加
+            tmp_win_list.append(tmp_part)
 
     # connpassからダウンロードできるcsvファイルはShift-JIS
-    with open(csv_path, encoding='cp932') as f:
+    with open(part_csv_path, encoding='cp932') as f:
         reader = csv.reader(f)
         # 1行目のヘッダーをスキップ
         next(reader)
@@ -35,19 +70,59 @@ def load(csv_path=''):
             tmp_part.status_part = row[5]
             # 出欠ステータス
             tmp_part.status_att = row[6]
+            # PayPal取引ID
+            tmp_part.paypal_id = row[7]
+            # 請求書ID
+            tmp_part.invoice_id = row[8]
             # 更新日時
-            tmp_part.update_time = row[7]
+            tmp_part.update_time = row[9]
             # 受付番号
-            tmp_part.rcpt_number = row[8]
+            tmp_part.rcpt_number = row[10]
 
+            # 更新日時,受付番号
             # 参加ステータスがキャンセルの参加者は除外
             if 'キャンセル' in tmp_part.status_part:
                 continue
 
-            # リストに参加者を追加
-            tmp_list.append(tmp_part)
+            # 当選者に含まれているため除外
+            flag = False
+            for tmp_win in tmp_win_list:
+                if tmp_win.user_name == tmp_part.user_name:
+                    flag = True
+                    continue
 
-        return tmp_list
+            if flag:
+                continue
+
+            # リストに参加者を追加
+            tmp_part_list.append(tmp_part)
+
+        return tmp_part_list, tmp_win_list
+
+
+def save(csv_path, part_list):
+    # 当選者の読み込み
+    with open(csv_path, 'w', encoding='cp932', newline='') as f:
+        writer = csv.writer(f)
+        rows = []
+        for part in part_list:
+            row = [
+                part.frame_name,
+                part.user_name,
+                part.display_name,
+                part.start_date,
+                part.comment,
+                part.status_part,
+                part.status_att,
+                part.paypal_id,
+                part.invoice_id,
+                part.update_time,
+                str(part.rcpt_number)
+            ]
+            rows.append(row)
+
+        print(rows)
+        writer.writerows(rows)
 
 
 class Participant:
@@ -66,6 +141,10 @@ class Participant:
         self.status_part = ''
         # 出欠ステータス
         self.status_att = ''
+        # PayPal取引ID
+        self.paypal_id = ''
+        # 請求書ID
+        self.invoice_id = ''
         # 更新日時
         self.update_time = ''
         # 受付番号
